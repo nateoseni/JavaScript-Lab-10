dd65c32d33e31b5618b04723bab35a022f4ffe01
Nate Oseni <nateoseni@gmail.com>
2026-03-23 17:08:30 -0400

    Added more comments to explain thought proccesses

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 class ProductProperties {
+
+    //initialize a constructor to create name, price, and quantity objects
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    // Returns the total value of stock for this specific item
+    //returns the total value of stock for this specific item
     getTotalValue() {
         return this.price * this.quantity;
     }
 
-    // Returns a formatted string of the product details
+    //returns a formatted string of the product details
     toString() {
         return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
@@ -24,16 +26,20 @@ class ProductProperties {
 }
 
 class PerishableProductProperties extends ProductProperties{
+
+    //initialize a constructor with the values from parent class, and a property expirationDate
     constructor(name, price, quantity, expirationDate){
         super(name, price, quantity);
         this.expirationDate = expirationDate;
     }
+    
+    //override toString to include the expiration date property
     toString() {
         return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
     }
 }
 
-// Create two instances of this class with sample data and test output
+//create two instances of this class with sample data and test output
 
 const glue = new PerishableProductProperties("Glue", 2.23, 4, "03/23/2026");
 const balloon = new PerishableProductProperties("Balloon", 1.50, 10, "10/25/2026");
